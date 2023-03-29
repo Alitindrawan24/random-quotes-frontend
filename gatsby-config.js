7c0bb4bb88f3f8115960506a70fa9bc62486df8a
Alit Indrawan <alitindrawan71@gmail.com>
2023-03-29 23:47:05 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `random-quotes`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
